927fa0ef785e928fe2c02427bdd1097cf628c658
Javier Carrillo <javiercarrillo526@hotmail.com>
2024-06-14 11:10:40 -0300

    todoResolver-Mutation

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,8 +1,10 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver, Mutation } from '@nestjs/graphql';
 import { Todo } from './entity/todo.entity';
 import { TodoService } from './todo.service';
+import { CreateTodoInput } from './dto/inputs/create-todo.input';
+import { UpdateTodoInput } from './dto/inputs/update-todo.input';
 
-@Resolver()
+@Resolver( () => Todo ) /* Le decimos al resolver que va a estar trabajando con un objeto Todo */
 export class TodoResolver {
 
     constructor (
@@ -20,22 +22,26 @@ export class TodoResolver {
         return this.todoService.findOne ( id );
     }
 
-    createTodo() {
-
+    @Mutation( ()=> Todo, { name: 'createTodo' }  )
+    createTodo(
+        @Args('createTodoInput') createTodoInput: CreateTodoInput
+    )   {
+        return this.todoService.create( createTodoInput );
     }
 
-    updateTodo() {
-
+    @Mutation( ()=> Todo, { name: 'updateTodo' }  )
+    updateTodo( @Args('updateTodoInput') updateTodoInput: UpdateTodoInput )    /* Args: el nombre por el cual el front va a enviar los parámetros */
+        {
+            return this.todoService.update(updateTodoInput)
+    
     }
 
-    removeTodo() {
-
+    @Mutation( () => Boolean )
+    removeTodo(
+        @Args('id', { type: () => Int }) id: number
+    ) {
+        return this.todoService.delete( id );
     }
-
-
-
-
-
 
 
 
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo } from './entity/todo.entity';
+import { CreateTodoInput } from './dto/inputs/create-todo.input';
+import { UpdateTodoInput } from './dto/inputs/update-todo.input';
 
 @Injectable()
 export class TodoService {
@@ -23,7 +25,42 @@ export class TodoService {
         if ( !todo ) throw new NotFoundException(`Todo with id ${ id } not found`);
 
         return todo;
+    
+    }
 
+    create( createTodoInput: CreateTodoInput ): Todo {
+
+        const todo = new Todo();
+        todo.description = createTodoInput.description;
+        todo.id = Math.max( ...this.todos.map( todo=> todo.id ), 0 ) + 1
+        
+        this.todos.push( todo );
+
+        return todo;
+
+    }
+
+    update( updateTodoInput: UpdateTodoInput ) {
+        const { id, description, done } = updateTodoInput;
+        const todoToUpdate = this.findOne( id )
+
+        if ( description ) todoToUpdate.description = description;
+        if ( done !== undefined ) todoToUpdate.done = done;
+
+        this.todos = this.todos.map( todo => {
+            return ( todo.id === id ) ? todoToUpdate : todo;
+        });
+
+        return todoToUpdate;
+
+    }
+
+    delete( id:number ): Boolean {
+        const todo = this.findOne( id );
+
+        this.todos = this.todos.filter( todo => todo.id !== id )
+
+        return true;
     }
 
 
